4f1bd7b0569df5c5bab6427215a762693dc632b4
kcarullo <35302366+kcarullo@users.noreply.github.com>
2019-09-12 20:37:38 -0400

    Add files via upload

--- Supervised_DL/rnn/rnn.py
+++ Supervised_DL/rnn/rnn.py
@@ -72,8 +72,23 @@ dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
 # Getting the real stock price of 2017
-
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i - 60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
 # Visualizing the results
-
-
+plt.plot(real_stock_price, color = 'r', label = 'Real Google Stock Price')
+plt.plot(predicted_stock_price, color = 'b', label = 'Predicted Google Stock Price')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
